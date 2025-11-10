5472d2865b7edf8b3ecc7c24346203e59978ec29
Mike Taylor <194648486+mike-taylor2@users.noreply.github.com>
2025-11-10 16:14:40 -0700

    phase 5: Creating the separate UI's

---
+++ client/src/main/java/ChessClient.java
@@ -0,0 +1,15 @@
+import server.ServerFacade;
+import ui.PreLoginUI;
+
+public class ChessClient {
+    private final PreLoginUI preUI;
+
+    public ChessClient(String serverUrl){
+        ServerFacade server = new ServerFacade(serverUrl);
+        preUI = new PreLoginUI(server);
+    }
+
+    public void run(){
+        preUI.run();
+    }
+}
---
+++ client/src/main/java/ui/PreLoginUI.java
@@ -0,0 +1,17 @@
+package ui;
+
+import server.ServerFacade;
+
+public class PreLoginUI {
+    private final ServerFacade server;
+
+
+    public PreLoginUI(ServerFacade server) {
+        this.server = server;
+    }
+
+
+    public void run() {
+        
+    }
+}
