544fe1187b1ac4e53a2e6a71529ec3a37ef17dc8
Mike Taylor <194648486+mike-taylor2@users.noreply.github.com>
2025-11-14 11:17:24 -0700

    phase 5: Fixed Model

--- server/src/main/java/model/ListGamesResult.java
+++
@@ -1,6 +0,0 @@
-package model;
-
-import java.util.ArrayList;
-
-public record ListGamesResult(ArrayList<GameData> games) {
-}
--- server/src/main/java/model/LoginResult.java
+++
@@ -1,4 +0,0 @@
-package model;
-
-public record LoginResult(String username, String authToken) {
-}
