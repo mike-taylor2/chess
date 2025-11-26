b66512c3b03a93a4dcb204a83f3ee32032f4946e
Mike Taylor <194648486+mike-taylor2@users.noreply.github.com>
2025-11-26 10:56:20 -0700

    phase 6: ServerMessage architecture created

--- server/src/main/java/server/websocket/ConnectionManager.java
+++ server/src/main/java/server/websocket/ConnectionManager.java
@@ -7,11 +7,20 @@ import java.io.IOException;
 import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
 
+import static websocket.messages.ServerMessage.ServerMessageType.ERROR;
+
 public class ConnectionManager {
     public final ConcurrentHashMap<Integer, Set<Session>> connections = new ConcurrentHashMap<>();
 
     public void join(int GameID, Session session) {
-
+        for (Integer i : connections.keySet()) {
+            if (i.equals(GameID)) {
+                connections.get(i).add(session);
+                return;
+            }
+        }
+        var errorMessage = new ServerMessage(ERROR, "Error: gameID does not exist");
+        directedMessage(session, "Error: gameID")
     }
 
     public void leave(int GameID, Session session) {
